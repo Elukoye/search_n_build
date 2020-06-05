b16abdb4de5ecad1f29280f15e89bbb960bff753
Elukoye <lukoyedith@gmail.com>
2020-06-05 18:07:50 +0300

    create appointment model and appointment d.b table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_145152) do
+ActiveRecord::Schema.define(version: 2020_06_05_150159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "date"
+    t.bigint "user_id", null: false
+    t.bigint "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_appointments_on_project_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -30,4 +40,6 @@ ActiveRecord::Schema.define(version: 2020_06_05_145152) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "projects"
+  add_foreign_key "appointments", "users"
 end
