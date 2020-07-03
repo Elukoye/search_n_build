7fb07ee245346c54cdd6b50ae61a04f0c20536fe
Elukoye <lukoyedith@gmail.com>
2020-07-03 16:03:42 +0300

    fix rubocop linter errors

--- spec/requests/projects/post_request_spec.rb
+++ spec/requests/projects/post_request_spec.rb
@@ -5,8 +5,11 @@ require 'rails_helper'
 RSpec.describe 'POST /api/v1/projects', type: :request do
   it 'returns a 201 status code response' do
     post '/api/v1/projects',
-         params: { project: { title: 'Build a rails music board',
-                              description: 'Build a rails music board', hrs: 2.5 } }
+         params: { project: {
+           title: 'Build a rails music board',
+           description: 'Build a rails music board',
+           hrs: 2.5
+         } }
     expect(response).to have_http_status(:created)
   end
 end
--- spec/requests/users/post_request_spec.rb
+++ spec/requests/users/post_request_spec.rb
@@ -5,7 +5,10 @@ require 'rails_helper'
 RSpec.describe 'POST /api/v1/users', type: :request do
   it 'returns a 201 status code response' do
     post '/api/v1/users',
-         params: { user: { username: 'Bridget', password: '123456' } }
+         params: { user: {
+           username: 'Bridget',
+           password: '123456'
+         } }
     expect(response).to have_http_status(:created)
   end
 end
