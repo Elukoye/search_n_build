3ce7a4ce137d11a7cd7fd8dbd1015b0a478a6ae7
Elukoye <lukoyedith@gmail.com>
2020-07-07 18:13:12 +0300

    add factories to factories folder and api_helper to support folder

---
+++ spec/factories/projects.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :project do
+      title { Faker::Lorem.sentence(word_count: 3) }
+      description { Faker::Lorem.sentences(number: 1) }
+      hrs {2.5}
+    end
+end
