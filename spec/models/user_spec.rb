258b504b8bf2f053efe6c727d03bf7c2c541f62f
Elukoye <lukoyedith@gmail.com>
2020-07-10 22:54:45 +0300

    add user model tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+    it 'creates new User' do
+        new_user = create(:user)
+        expect(new_user).to be_valid
+    end
+
+    it 'is invalid if username is empty' do
+        nil_username = create(:user)
+        nil_username.username = nil
+        expect(nil_username).not_to be_valid
+    end
+
+    
+    it 'is invalid if password is empty' do
+        nil_username = create(:user)
+        nil_username.password = nil
+        expect(nil_username).not_to be_valid
+    end
+end
