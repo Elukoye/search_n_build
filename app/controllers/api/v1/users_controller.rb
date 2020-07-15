682a120cc82a7b2a6dcb696d9a827f53070f2b0c
Elukoye <lukoyedith@gmail.com>
2020-07-15 19:59:05 +0300

    modify before action method in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authorize_request, only: [:create,:destroy]
+  before_action :authorize_request, only: [:index,:destroy]
   
   def index
     @users = User.all
