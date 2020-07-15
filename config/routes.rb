c4c965715609db1093a41598ee23737127b5be4c
Elukoye <lukoyedith@gmail.com>
2020-07-15 19:11:47 +0300

    refactor route namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[create index destroy]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       resources :sessions, only: %i[create]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       resources :projects, only: %i[create index show destroy]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       resources :appointments
     end
   end
